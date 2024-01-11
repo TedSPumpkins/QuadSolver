8f1cea83aa45cf6500948d1ace28f16bbc986f11
Theodore Rountree-Shtulman <trountree-shtulman24@students.polytechnic.org>
2024-01-11 09:15:27 -0800

    hvbbu

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,5 +1,5 @@
 public class QuadraticSolver{
     public static void main(String[] args){
-        System.out.println("Welcome to the Quadratic Solver!");
+        System.out.println("Welcome to the Quadratic solver!");
     }
 }
